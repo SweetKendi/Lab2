56d837d810b8315ff42877e2052c6bf9fad7cace
james <ejpasco.22@ichat.sp.edu.sg>
2023-04-27 16:43:10 +0800

    changes made

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,27 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("enter some numbers seperated by commas ")
+
+def calc_average(x):
+    y = sum(x)/len(x)
+    return y
+
+def get_user_input():
+    x=input().split(",")
+    y=[]
+    for i in x:
+        y.append(float(i))
+    return y
+
+def find_min_max():
+
+
+
+def sort_temp():
+    print("sort_temp")
+def calc_median_temp():
+    print("calc_median_temp")
+
+
+
+
+
---
+++ bmi2.0.py
@@ -0,0 +1,4 @@
+height = float(input("What is your weight in kg? "))
+weight = float(input("What is your height in metres? "))
+bmi = weight / (height ** 2)
+print("your bmi is ",round(bmi,2))
