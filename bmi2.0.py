b2ef28ddd7c52fcde548c4e60cdc1de8b3094f99
james <ejpasco.22@ichat.sp.edu.sg>
2023-05-04 14:08:52 +0800

    changes made

--- bmi2.0.py
+++ bmi2.0.py
@@ -1,4 +1,9 @@
-height = float(input("What is your weight in kg? "))
-weight = float(input("What is your height in metres? "))
-bmi = weight / (height ** 2)
-print("your bmi is ",round(bmi,2))
+height = float(input("What is your height in m? "))
+weight = float(input("What is your weight in kg? "))
+bmi = weight / (height**2)
+if bmi<18.5:
+    print("Your bmi is ",round (bmi, 2),"and you are under weight")
+if bmi>25.0:
+    print("Your bmi is ",round(bmi, 2), "and you are over weight")
+else:
+    print("Your bmi is ",round(bmi,2), "and you are normal weight")
