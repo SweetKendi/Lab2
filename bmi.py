af26891dcd4fbe2298686485f567ff817bc0f5e1
james <ejpasco.22@ichat.sp.edu.sg>
2023-05-04 14:34:39 +0800

    Changes Made

--- bmi.py
+++ bmi.py
@@ -5,13 +5,18 @@ def calculate_bmi(height, weight):
     print("BMI = ", bmi)
     if bmi < 18.5:
         print("Your bmi is ", bmi, "and you are under weight")
+        return -1
     if bmi > 25.0:
         print("Your bmi is " , bmi , "and you are over weight")
+        return 1
     else:
         print("your bmi is " , bmi , "and you are normal weight")
+        return 0
 
 
-calculate_bmi(weight=57,height=1.73)
+
+x=  calculate_bmi(weight=57,height=1.73)
+print(x)
 
 
 
